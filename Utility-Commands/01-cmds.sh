40fdc9af4cf095527a459a72f2389c91c8d52df2
Dilip Rathod <rathod.dt@gmail.com>
2024-07-28 01:24:41 +0530

    Utility commands

---
+++ Utility-Commands/01-cmds.sh
@@ -0,0 +1,19 @@
+date
+uptime
+hostname
+uname
+uname -a
+which pwd
+ls -l /usr/bin | wc -l
+cal
+cal 10 1982
+cal 2016
+[vagrant@localhost ~]$ bc
+bc 1.07.1
+Copyright 1991-1994, 1997, 1998, 2000, 2004, 2006, 2008, 2012-2017 Free Software Foundation, Inc.
+This is free software with ABSOLUTELY NO WARRANTY.
+For details type `warranty'.
+2+3
+5
+quit
+
---
+++ Utility-Commands/13-arch.sh
@@ -0,0 +1,4 @@
+[vagrant@localhost ~]$ arch
+x86_64
+[vagrant@localhost ~]$ uname -m
+x86_64
