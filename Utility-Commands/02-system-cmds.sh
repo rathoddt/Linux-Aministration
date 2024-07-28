7ca628a7ce88176530c95922989025b514f61c1b
Dilip Rathod <rathod.dt@gmail.com>
2024-07-29 00:27:06 +0530

    Process management bg, fg, nohup and nice

--- Utility-Commands/02-system-cmds.sh
+++ Utility-Commands/02-system-cmds.sh
@@ -37,3 +37,25 @@ crontab -e # Edit the  crontab entry
 crontab -l # List crontab entries
 crontab -r # Remove contab entries
 crond      # crontab daemon/service that manages cron entries
+#Hourly, Daily, Weekly and Monthly job exeuted
+[root@localhost ~]# ls -l /etc | grep cron
+-rw-r--r--. 1 root root      541 Nov 30  2023 anacrontab
+drwxr-xr-x. 2 root root       21 Jul  5 14:09 cron.d
+drwxr-xr-x. 2 root root        6 Aug  9  2021 cron.daily
+-rw-r--r--. 1 root root        0 Nov 30  2023 cron.deny
+drwxr-xr-x. 2 root root       22 Jul  5 14:09 cron.hourly
+drwxr-xr-x. 2 root root        6 Aug  9  2021 cron.monthly
+-rw-r--r--. 1 root root      451 Aug  9  2021 crontab
+drwxr-xr-x. 2 root root        6 Aug  9  2021 cron.weekly
+
+#-------------------------------
+at HH:MM PM
+atq
+atrm # 
+systemctl status atd 
+
+at 2:45 AM 101621 # Schedule job at Oct 16 2021
+at 4PM + 4 days
+at now + 5 hours
+at 8:00 AM Sun
+at 10:00 AM next month
---
+++ Utility-Commands/03-process-management.sh
@@ -0,0 +1,8 @@
+#-------------------------------
+Ctrl+Z # stop the job
+bg # run job(stopped) in backgrounf
+jobs # view jobs
+fg #bring job to foreground
+nohup sleep 150 &  # run process even after exit(window closed)
+
+nice -n 5 sleep 10
