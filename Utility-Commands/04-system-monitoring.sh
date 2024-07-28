efad5bae2321b09b03ad5ea042d96739c3cf0969
Dilip Rathod <rathod.dt@gmail.com>
2024-07-29 00:57:50 +0530

    Log monitoring

---
+++ Utility-Commands/04-system-monitoring.sh
@@ -0,0 +1,10 @@
+top 
+df
+dmesg 
+iostat 1
+netstat
+netstat -rnv
+
+free 
+cat /proc/cpuinfo
+cat /proc/meminfo 
---
+++ Utility-Commands/05-log-monitoring.sh
@@ -0,0 +1,9 @@
+#Log directory  = /var/log
+# different logs
+boot
+chronyd = NTP
+cron 
+maillog
+secure 
+messages
+httpd
